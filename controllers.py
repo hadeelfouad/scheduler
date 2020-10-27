1e6e740c31b0413bc996d475565647699fbd4439
fouadh <hadeel.fouad@emc.com>
2020-10-27 02:05:16 +0200

    stop loop on possible shifts

--- controllers.py
+++ controllers.py
@@ -22,7 +22,6 @@ def create_schedule():
     schedule['Date'] = pd.to_datetime(schedule['Date']).dt.date
     schedule['Shift End'] = schedule['Shift Start']  + pd.to_timedelta(8, unit='h')
     schedule['Hours'] = schedule['Quantity'] * 8
-    print(schedule.head())
 
     matches['Availability'] = pd.to_datetime(matches['Match Date']+ ' ' + matches['Kick-off Time'], format='%d/%m/%Y %H:%M') + pd.DateOffset(minutes=110)
     matches = matches.merge(competitions[['Priority', 'Competition']], how='left').merge(priorities, how='left', left_on='Priority', right_on='Priority Class').drop(labels='Priority Class', axis=1)
@@ -32,11 +31,9 @@ def create_schedule():
     matches.sort_values(by=['Deadline', 'Availability'], inplace=True)
 
     for i in matches.index:
-        print('***********MATCH ID: ' + str(matches.loc[i, 'ID']))
-        possible_squads =  schedule.loc[schedule['Shift End'] - matches.loc[i, "Availability"] >= pd.to_timedelta(4, unit='h')]
+        possible_squads =  schedule.loc[(schedule['Shift End'] > matches.loc[i, "Availability"]) & (schedule['Shift End'] - matches.loc[i, "Availability"] >= pd.to_timedelta(4, unit='h'))]
         possible_squads =  possible_squads[(schedule['Shift Start'] <= matches.loc[i, "Deadline"]) & (schedule['Hours'] > 0)]
         if possible_squads.empty:
-            print('no squad' + str(matches.loc[i, 'ID']))
             output = pd.concat([output, pd.DataFrame({"Match ID": [matches.loc[i, 'ID']],
                                                         'Match Availability':[matches.loc[i, 'Availability']],
                                                         'Match Deadline':[matches.loc[i, 'Deadline']],
@@ -45,7 +42,6 @@ def create_schedule():
         possible_squads.sort_values(by=['Date', 'Shift'], inplace=True)
         time_to_deadline = pd.to_timedelta([matches.loc[i, 'Deadline'] - possible_squads.loc[possible_squads.index[0], 'Shift Start']]).astype('timedelta64[h]')[0]
         if time_to_deadline < 4:
-            print('deadline' + str(matches.loc[i, 'ID']))
             output = pd.concat([output, pd.DataFrame({"Match ID": [matches.loc[i, 'ID']],
                                                         'Match Availability':[matches.loc[i, 'Availability']],
                                                         'Match Deadline':[matches.loc[i, 'Deadline']],
@@ -56,19 +52,19 @@ def create_schedule():
         
         shifts_group = possible_squads.groupby(['Date','Shift'])
         for key, _ in shifts_group:
+            if matches.loc[i, 'Remaining Hours'] == 0:
+                break
             shift = shifts_group.get_group(key)
             # get squads whose one of their preference is the match 
-            prefered_squad = shift[(shift['Squad'] == matches.loc[i, 'Squad']) & (shift['Hours'] > 0)]
+            prefered_squad = shift[(shift['Squad'] == matches.loc[i, 'Squad']) & (shift['Quantity'] > 0)]
             # assigned match to one of the squads that prefer it if any
             if not prefered_squad.empty:
-                print('preference squad:')
                 # ther can be only one prefered time per shift as shown by the data in preferences.csv
                 prefered_squad_index = prefered_squad.index[0]
-                print(prefered_squad.loc[prefered_squad_index])
                 
 
                 if prefered_squad.loc[prefered_squad_index, 'Shift End'] - matches.loc[i, "Availability"] >= pd.to_timedelta(8, unit='h'):
-                    if prefered_squad.loc[prefered_squad_index, 'Hours'] >= 8:
+                    if prefered_squad.loc[prefered_squad_index, 'Quantity'] >= 1:
                         hours_worked = matches.loc[i, "Remaining Hours"]
                         prefered_squad.at[prefered_squad_index, 'Hours'] = prefered_squad.loc[prefered_squad_index, 'Hours'] - hours_worked
                         shift.at[prefered_squad_index, 'Hours'] = shift.loc[prefered_squad_index, 'Hours'] - hours_worked
@@ -88,30 +84,33 @@ def create_schedule():
                                                             'Shift': [prefered_squad.loc[prefered_squad_index, 'Shift']], 
                                                             'Hours/Shift': [hours_worked],
                                                             'Done': [matches.loc[i, 'Remaining Hours'] == 0]})])
-                        print('==================================')
 
                 elif prefered_squad.loc[prefered_squad_index, 'Shift End'] - matches.loc[i, "Availability"] >= pd.to_timedelta(4, unit='h'):
-                    if prefered_squad.loc[prefered_squad_index, 'Hours'] >= matches.loc[i, "Remaining Hours"]:
-                        needed_members = (matches.loc[i, "Remaining Hours"])/4
-                        if prefered_squad.loc[prefered_squad_index, 'Quantity'] >= needed_members:
-                            hours_worked = needed_members * 4
-                            prefered_squad.at[prefered_squad_index, 'Hours'] = prefered_squad.loc[prefered_squad_index, 'Hours'] - hours_worked
-                            shift.at[prefered_squad_index, 'Hours'] = shift.loc[prefered_squad_index, 'Hours'] - hours_worked
-                            schedule.at[prefered_squad_index, 'Hours'] = schedule.loc[prefered_squad_index, 'Hours'] - hours_worked
-                            matches.at[i, 'Remaining Hours'] = matches.loc[i, 'Remaining Hours'] - hours_worked
-                            prefered_squad.at[prefered_squad_index, 'Quantity'] = prefered_squad.loc[prefered_squad_index, 'Quantity'] - needed_members
-                            shift.at[prefered_squad_index, 'Quantity'] = shift.loc[prefered_squad_index, 'Quantity'] - needed_members
-                            schedule.at[prefered_squad_index, 'Quantity'] = schedule.loc[prefered_squad_index, 'Quantity'] - needed_members
-                            output = pd.concat([output, pd.DataFrame({'Match ID':[matches.loc[i, 'ID']],
-                                                            'Match Availability':[matches.loc[i, 'Availability']],
-                                                            'Match Deadline':[matches.loc[i, 'Deadline']],
-                                                            'Squad': [prefered_squad.loc[prefered_squad_index, 'Squad']],
-                                                            'Members/Match': [needed_members],
-                                                            'Date': [prefered_squad.loc[prefered_squad_index, 'Date']], 
-                                                            'Shift': [prefered_squad.loc[prefered_squad_index, 'Shift']], 
-                                                            'Hours/Shift': [hours_worked],
-                                                            'Done': [matches.loc[i, 'Remaining Hours'] == 0]})])
-                            print('---------------------------------------------')
+                    # if prefered_squad.loc[prefered_squad_index, 'Hours'] >= matches.loc[i, "Remaining Hours"]:
+                    # if prefered_squad.loc[prefered_squad_index, 'Quantity'] == 0:
+                    #     break
+                    needed_members = (matches.loc[i, "Remaining Hours"])/4
+                    if prefered_squad.loc[prefered_squad_index, 'Quantity'] >= needed_members:
+                        hours_worked = needed_members * 4
+                    elif prefered_squad.loc[prefered_squad_index, 'Quantity'] == 1:
+                        hours_worked = 4
+                        needed_members = 1
+                    prefered_squad.at[prefered_squad_index, 'Hours'] = prefered_squad.loc[prefered_squad_index, 'Hours'] - hours_worked
+                    shift.at[prefered_squad_index, 'Hours'] = shift.loc[prefered_squad_index, 'Hours'] - hours_worked
+                    schedule.at[prefered_squad_index, 'Hours'] = schedule.loc[prefered_squad_index, 'Hours'] - hours_worked
+                    matches.at[i, 'Remaining Hours'] = matches.loc[i, 'Remaining Hours'] - hours_worked
+                    prefered_squad.at[prefered_squad_index, 'Quantity'] = prefered_squad.loc[prefered_squad_index, 'Quantity'] - needed_members
+                    shift.at[prefered_squad_index, 'Quantity'] = shift.loc[prefered_squad_index, 'Quantity'] - needed_members
+                    schedule.at[prefered_squad_index, 'Quantity'] = schedule.loc[prefered_squad_index, 'Quantity'] - needed_members
+                    output = pd.concat([output, pd.DataFrame({'Match ID':[matches.loc[i, 'ID']],
+                                                    'Match Availability':[matches.loc[i, 'Availability']],
+                                                    'Match Deadline':[matches.loc[i, 'Deadline']],
+                                                    'Squad': [prefered_squad.loc[prefered_squad_index, 'Squad']],
+                                                    'Members/Match': [needed_members],
+                                                    'Date': [prefered_squad.loc[prefered_squad_index, 'Date']],
+                                                    'Shift': [prefered_squad.loc[prefered_squad_index, 'Shift']],
+                                                    'Hours/Shift': [hours_worked],
+                                                    'Done': [matches.loc[i, 'Remaining Hours'] == 0]})])
 
 
                         
@@ -147,8 +146,8 @@ def create_schedule():
                 #                                             'Hours/Shift': [4],
                 #                                             'Done': [matches.loc[i, 'Remaining Hours'] == 0]})])
             # check if prefered squad was able to finish the match
-            # if matches.loc[i, 'Remaining Hours'] == 0:
-            #     break
+            if matches.loc[i, 'Remaining Hours'] == 0:
+                break
             # shift = shift[shift['Hours'] > 0] 
             if not prefered_squad.empty:
                 shift = shift.drop(index=prefered_squad.index)
@@ -173,10 +172,10 @@ def create_schedule():
                                                                     'Hours/Shift': [10],
                                                                     'Done': [matches.loc[i, 'Remaining Hours'] == 0]})])
                     # worked on one team per match
-                    elif shift.loc[index, 'Quantity'] >= 5 and matches.loc[i, 'Remaining Hours'] == 4: # check on quantity instead of hours
+                    elif shift.loc[index, 'Quantity'] >= 1: # check on quantity instead of hours
                         matches.at[i, 'Remaining Hours'] = matches.loc[i, 'Remaining Hours'] - 4
-                        shift.at[index, 'Hours'] = shift.loc[prefered_squad_index, 'Hours'] - 5
-                        schedule.at[index, 'Hours'] = schedule.loc[prefered_squad_index, 'Hours'] - 5
+                        shift.at[index, 'Hours'] = shift.loc[index, 'Hours'] - 5
+                        schedule.at[index, 'Hours'] = schedule.loc[index, 'Hours'] - 5
                         shift.at[index, 'Quantity'] = shift.loc[index, 'Quantity'] - 1
                         schedule.at[index, 'Quantity'] = schedule.loc[index, 'Quantity'] - 1
                         output = pd.concat([output, pd.DataFrame({'Match ID':[matches.loc[i, 'ID']],
@@ -189,10 +188,7 @@ def create_schedule():
                                                                     'Hours/Shift': [5],
                                                                     'Done': [matches.loc[i, 'Remaining Hours'] == 0]})])
                 else:
-                    if len(output.loc[(output['Match ID'] == matches.loc[i, 'ID'])].index) == 0:
-                        print('match does not statisfy any shift')
-                        print(matches.loc[i, 'ID'])
-                        print('$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$')
+                    if index == shift.index[-1] & len(output.loc[(output['Match ID'] == matches.loc[i, 'ID'])].index) == 0:
                         output = pd.concat([output, pd.DataFrame({"Match ID": [matches.loc[i, 'ID']],
                                                         'Match Availability':[matches.loc[i, 'Availability']],
                                                         'Match Deadline':[matches.loc[i, 'Deadline']],
